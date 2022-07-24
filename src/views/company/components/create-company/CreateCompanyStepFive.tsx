4a739a163c471a153247e0d86ae0194ba918ff3d
kaoths <tanawit139@gmail.com>
2022-07-24 22:59:47 +0700

    feat: copy address add funds card

--- src/views/company/components/create-company/CreateCompanyStepFive.tsx
+++ src/views/company/components/create-company/CreateCompanyStepFive.tsx
@@ -12,56 +12,58 @@ export const CreateCompanyStepFive = observer(() => {
   const { data: vaults, loading: isVaultsLoading, error: isVaultsError } = useUserVaults();
 
   return (
-    <Box width="100%">
-      <Grid container columnSpacing={3.5} rowSpacing={3.5}>
-        {vaults?.length > 0 &&
-          vaults.map((vault, index) => {
-            return (
-              <Grid key={index} item xs={12} sm={4}>
-                <VaultAddFundsCard vault={vault} />
-              </Grid>
-            );
-          })}
-      </Grid>
-      <Box
-        width="100%"
-        maxWidth={1000}
-        display="flex"
-        justifyContent="space-between"
-        my={5}
-        mx="auto"
-      >
-        <Stack direction="row" spacing={2}>
-          <Button
-            variant="text"
-            startIcon={
-              <Box>
-                <FontAwesomeIcon icon={faChevronLeft} size="xs" />
-              </Box>
-            }
-            onClick={createCompanyStore.previousStep}
-          >
-            <Typography variant="button" color="primary">
-              Back
-            </Typography>
-          </Button>
-          <Button variant="text" onClick={createCompanyStore.reset}>
-            <Typography variant="button" color="primary">
-              Start Over
-            </Typography>
-          </Button>
-        </Stack>
-        <Link href={`/company/${createCompanyStore.companySlug}/dashboard`} passHref>
-          <PrimaryGradientButton
-            disabled={!createCompanyStore.companyName}
-            onClick={createCompanyStore.nextStep}
-          >
-            <Typography variant="button" color="textPrimary">
-              Continue
-            </Typography>
-          </PrimaryGradientButton>
-        </Link>
+    <>
+      <Box width="100%">
+        <Grid container columnSpacing={3.5} rowSpacing={3.5}>
+          {vaults?.length > 0 &&
+            vaults.map((vault, index) => {
+              return (
+                <Grid key={index} item xs={12} sm={4}>
+                  <VaultAddFundsCard vault={vault} />
+                </Grid>
+              );
+            })}
+        </Grid>
+        <Box
+          width="100%"
+          maxWidth={1000}
+          display="flex"
+          justifyContent="space-between"
+          my={5}
+          mx="auto"
+        >
+          <Stack direction="row" spacing={2}>
+            <Button
+              variant="text"
+              startIcon={
+                <Box>
+                  <FontAwesomeIcon icon={faChevronLeft} size="xs" />
+                </Box>
+              }
+              onClick={createCompanyStore.previousStep}
+            >
+              <Typography variant="button" color="primary">
+                Back
+              </Typography>
+            </Button>
+            <Button variant="text" onClick={createCompanyStore.reset}>
+              <Typography variant="button" color="primary">
+                Start Over
+              </Typography>
+            </Button>
+          </Stack>
+          <Link href={`/company/${createCompanyStore.companySlug}/dashboard`} passHref>
+            <PrimaryGradientButton
+              disabled={!createCompanyStore.companyName}
+              onClick={createCompanyStore.nextStep}
+            >
+              <Typography variant="button" color="textPrimary">
+                Continue
+              </Typography>
+            </PrimaryGradientButton>
+          </Link>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 });
--- src/views/company/components/create-company/VaultAddFundsCard.tsx
+++ src/views/company/components/create-company/VaultAddFundsCard.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import {
+  Alert,
   Box,
   Button,
   IconButton,
   MenuItem,
   Paper,
   Skeleton,
+  Snackbar,
   Stack,
   Typography,
 } from "@mui/material";
@@ -23,14 +25,17 @@ import type { ContractAddressKey } from "src/constants/contracts";
 import { ERC20ABI } from "src/abis";
 import { ethers } from "ethers";
 import type { Vault } from "src/interfaces/vault";
+import { useClipboard } from "use-clipboard-copy";
 
 type Props = {
   vault: Vault;
 };
 export const VaultAddFundsCard: FC<Props> = ({ vault, ...props }) => {
+  const clipboard = useClipboard();
   const [qrCodeDataUrl, setQrCodeDataUrl] = useState<string | null>(null);
   const [transferAmount, setTransferAmount] = useState<string>("");
   const [selectedTokenSymbol, setSelectedTokenSymbol] = useState<ContractAddressKey>("kiusd");
+  const [isSnackbarOpen, setIsSnackbarOpen] = useState(false);
 
   const { account, provider } = useWeb3React();
   const { contractAddress } = useConstant();
@@ -69,100 +74,118 @@ export const VaultAddFundsCard: FC<Props> = ({ vault, ...props }) => {
   };
 
   return (
-    <Paper
-      elevation={0}
-      sx={{
-        borderRadius: 12,
-        backgroundColor: "#D7D7FD",
-        width: "100%",
-        maxWidth: 385,
-        minHeight: 400,
-        p: 3,
-      }}
-    >
-      <Stack spacing={2} alignItems="center">
-        <Typography sx={{ fontSize: 20, fontWeight: 600 }}>{vault.name}</Typography>
-        {qrCodeDataUrl ? (
-          <Box
-            component="img"
-            src={qrCodeDataUrl}
-            sx={{ width: 220, height: 220, borderRadius: 18 }}
-          />
-        ) : (
-          <Skeleton
-            variant="rectangular"
-            width={220}
-            height={220}
-            animation="wave"
-            sx={{ borderRadius: 18 }}
-          />
-        )}
-        <Stack spacing={0.5} width="100%" alignItems="flex-start">
-          <Typography sx={{ fontSize: 14, fontWeight: 600 }}>Vault Address</Typography>
-          <Box
-            display="flex"
-            width="100%"
-            justifyContent="space-between"
-            alignItems="center"
-            sx={{
-              color: "#6F6FE8",
-              px: 2,
-              borderRadius: 999,
-              backgroundColor: "#BBBBFF",
-            }}
-          >
-            <Typography variant="body2" color="inherit">
-              <b>{shortenAddress(vault.address)}</b>
-            </Typography>
-            <IconButton color="inherit" disableRipple>
-              <CopyIcon />
-              {/* <FontAwesomeIcon icon={faCopy} /> */}
-            </IconButton>
-          </Box>
-        </Stack>
-        <Stack spacing={0.5} width="100%" alignItems="flex-start">
-          <Typography variant="body2">
-            <b>Send Funds</b>
-          </Typography>
-          <Box width="100%" display="flex" justifyContent="space-between">
-            <GeneralOutlinedInput
-              placeholder="Enter Amount"
-              type="text"
-              sx={{ width: 130, "& .MuiOutlinedInput-input": { py: 1 } }}
-              InputProps={{
-                inputComponent: NumberFormatCurrency as any,
-              }}
-              onKeyDown={(e) => {
-                if (["ArrowDown", "ArrowUp"].includes(e.key)) {
-                  e.preventDefault();
-                }
-              }}
-              onWheel={(e) => (e.target as HTMLInputElement).blur()}
-              inputProps={{ style: { textAlign: "right", marginTop: 0 } }}
-              value={transferAmount}
-              onChange={(e) => setTransferAmount(e.target.value)}
+    <>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={isSnackbarOpen}
+        autoHideDuration={3000}
+        onClose={() => setIsSnackbarOpen(false)}
+      >
+        <Alert severity="success">Copied!</Alert>
+      </Snackbar>
+
+      <Paper
+        elevation={0}
+        sx={{
+          borderRadius: 12,
+          backgroundColor: "#D7D7FD",
+          width: "100%",
+          maxWidth: 385,
+          minHeight: 400,
+          p: 3,
+        }}
+      >
+        <Stack spacing={2} alignItems="center">
+          <Typography sx={{ fontSize: 20, fontWeight: 600 }}>{vault.name}</Typography>
+          {qrCodeDataUrl ? (
+            <Box
+              component="img"
+              src={qrCodeDataUrl}
+              sx={{ width: 220, height: 220, borderRadius: 18 }}
             />
-            <GeneralSelect
-              value={selectedTokenSymbol}
-              onChange={(e) => setSelectedTokenSymbol(e.target.value as any)}
-              sx={{ width: 100, "& .MuiOutlinedInput-input": { py: 1 } }}
+          ) : (
+            <Skeleton
+              variant="rectangular"
+              width={220}
+              height={220}
+              animation="wave"
+              sx={{ borderRadius: 18 }}
+            />
+          )}
+          <Stack spacing={0.5} width="100%" alignItems="flex-start">
+            <Typography sx={{ fontSize: 14, fontWeight: 600 }}>Vault Address</Typography>
+            <Box
+              display="flex"
+              width="100%"
+              justifyContent="space-between"
+              alignItems="center"
+              sx={{
+                color: "#6F6FE8",
+                px: 2,
+                borderRadius: 999,
+                backgroundColor: "#BBBBFF",
+              }}
             >
-              <MenuItem value="usdt">USDT</MenuItem>
-              <MenuItem value="usdc">USDC</MenuItem>
-              <MenuItem value="kiusd">KiUSD</MenuItem>
-            </GeneralSelect>
-            <Button
-              variant="contained"
-              sx={{ backgroundColor: "white", borderRadius: 999, px: 1.5 }}
-              onClick={handleTransferClick}
-            >
-              <Typography variant="body2">
-                <b>Transfer</b>
+              <Typography variant="body2" color="inherit">
+                <b>{shortenAddress(vault.address, 14)}</b>
               </Typography>
-            </Button>
-          </Box>
+              <IconButton
+                color="inherit"
+                disableRipple
+                onClick={() => {
+                  clipboard.copy(vault.address);
+                  setIsSnackbarOpen(true);
+                }}
+              >
+                <CopyIcon />
+                {/* <FontAwesomeIcon icon={faCopy} /> */}
+              </IconButton>
+            </Box>
+          </Stack>
+          <Stack spacing={0.5} width="100%" alignItems="flex-start">
+            <Typography variant="body2">
+              <b>Send Funds</b>
+            </Typography>
+            <Box width="100%" display="flex" justifyContent="space-between">
+              <GeneralOutlinedInput
+                placeholder="Enter Amount"
+                type="text"
+                sx={{ width: 130, "& .MuiOutlinedInput-input": { py: 1 } }}
+                InputProps={{
+                  inputComponent: NumberFormatCurrency as any,
+                }}
+                onKeyDown={(e) => {
+                  if (["ArrowDown", "ArrowUp"].includes(e.key)) {
+                    e.preventDefault();
+                  }
+                }}
+                onWheel={(e) => (e.target as HTMLInputElement).blur()}
+                inputProps={{ style: { textAlign: "right", marginTop: 0 } }}
+                value={transferAmount}
+                onChange={(e) => setTransferAmount(e.target.value)}
+              />
+              <GeneralSelect
+                value={selectedTokenSymbol}
+                onChange={(e) => setSelectedTokenSymbol(e.target.value as any)}
+                sx={{ width: 100, "& .MuiOutlinedInput-input": { py: 1 } }}
+              >
+                <MenuItem value="usdt">USDT</MenuItem>
+                <MenuItem value="usdc">USDC</MenuItem>
+                <MenuItem value="kiusd">KiUSD</MenuItem>
+              </GeneralSelect>
+              <Button
+                variant="contained"
+                sx={{ backgroundColor: "white", borderRadius: 999, px: 1.5 }}
+                onClick={handleTransferClick}
+              >
+                <Typography variant="body2">
+                  <b>Transfer</b>
+                </Typography>
+              </Button>
+            </Box>
+          </Stack>
         </Stack>
-      </Stack>
-    </Paper>
+      </Paper>
+    </>
   );
 };
